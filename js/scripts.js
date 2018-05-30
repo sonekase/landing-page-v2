4056b7d4b535cf19d84b84dceb7316e3472cbc07
Guest User <Guest@epicodus-5.local>
2018-05-30 08:05:49 -0700

    Add hidden feature in javascrip to hide interest, hobbies, skills, and to hide gallery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
     $("#initially-showing").fadeToggle();
-    
+  });
+  $(".clickable2").click(function() {
+    $("#initially-showing2").fadeToggle();
+  });
+  $(".clickable3").click(function() {
+    $("#initially-showing3").fadeToggle();
+  });
+  $(".clickable4").click(function() {
+    $("#initially-showing4").fadeToggle();
   });
 });
